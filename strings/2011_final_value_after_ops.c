609831a836f859903889872cee3483937ac9dc72
Weilin Luo <weilin.william.luo@gmail.com>
2021-11-10 14:23:36 -0800

    2011 final value

--- strings/2011_final_value_after_ops.c
+++ strings/2011_final_value_after_ops.c
@@ -47,7 +47,7 @@ operations[i] will be either "++X", "X++", "--X", or "X--".
 /*
 First version
 */
-int finalValueAfterOperations(char ** operations, int operationsSize){
+int finalValueAfterOperations_1(char ** operations, int operationsSize){
     int x = 0;
 
     for (int i = 0; i < operationsSize; i++) {
@@ -59,6 +59,46 @@ int finalValueAfterOperations(char ** operations, int operationsSize){
 
     return x;
 }
+
+/*
+Second version - Wrong!
+
+In C, we cannot use "==" or "!=" to compare two strings. The following function might seem to work though. This is because "==" actaully compares the base addresses of the two sides. Here operations[i] is a pointer to char that points to unnamed string literary "++X" and "X++", so they might return true. But the behaviour is undefined. When I ran this program locally it seems to pass but it failed to pass the online tester of Leetcode.
+*/
+int finalValueAfterOperations_2(char ** operations, int operationsSize){
+    int x = 0;
+
+    for (int i = 0; i < operationsSize; i++) {
+        if (operations[i] == "++X" || operations[i] == "X++")
+            x++;
+        else
+            x--;
+    }
+
+    return x;
+}
+
+/*
+Version 3
+
+Note that the second char of both "++X" and "X++" is '+' and the second char of both "--X" and "X--" is '-'.
+This observation should make the comparison a little easer!
+
+And note that a char in C is actually an integer between 1 and 255, and thus can be compared with comparison ops.
+*/
+int finalValueAfterOperations(char ** operations, int operationsSize){
+    int x = 0;
+
+    for (int i = 0; i < operationsSize; i++) {
+        if (operations[i][1] == '+')
+            x++;
+        else
+            x--;
+    }
+
+    return x;
+}
+
 
 /*
 Example 1:
