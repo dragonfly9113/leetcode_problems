9e308f48ad80b3d797f500e379a1f5e023ef43d0
Weilin Luo <weilin.william.luo@gmail.com>
2021-11-15 16:16:20 -0800

    1773 count items matching rule

--- strings/1773_count_items_matching.c
+++ strings/1773_count_items_matching.c
@@ -28,14 +28,33 @@ All strings consist only of lowercase letters.
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*
 Version 1
-Example 1:
-Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
-Output: 1
-Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
-ruleKey is equal to either "type", "color", or "name".
+This version works when items is defined/initialized as below:
+    char items1[][3][10] = {{"phone","blue","pixel"}, {"computer","silver","lenovo"}, {"phone","gold","iphone"}};
+
+This version can NOT pass the official tester because it does NOT math the required signature below:
+    int countMatches(char ***items, int itemsSize, int* itemsColSize, char * ruleKey, char * ruleValue);
+*/
+int countMatches_1(char items[][3][10], int itemsSize, int* itemsColSize, char * ruleKey, char * ruleValue){
+    int cnt = 0;
+    int itemsCol = 0;
+
+    if (!strcmp(ruleKey, "type")) itemsCol = 0;
+    else if (!strcmp(ruleKey, "color")) itemsCol = 1;
+    else itemsCol = 2;
+
+    for (int i = 0; i < itemsSize; i++) {
+        if (!strcmp(items[i][itemsCol], ruleValue)) cnt++;
+    }
+
+    return cnt;
+}
+
+/*
+Version 2
 */
 int countMatches(char ***items, int itemsSize, int* itemsColSize, char * ruleKey, char * ruleValue){
     int cnt = 0;
@@ -52,7 +71,24 @@ int countMatches(char ***items, int itemsSize, int* itemsColSize, char * ruleKey
     return cnt;
 }
 
-/*
+/* Version 1: NOT OK!
+This version of main() requires the follow countMtches(), which is NOT OK for the official tester:
+    int countMatches_1(char items[][3][10], int itemsSize, int* itemsColSize, char * ruleKey, char * ruleValue);
+*/
+int main_1() {
+    int count = 0;
+    int itemsColSize = 3;
+    char items1[][3][10] = {{"phone","blue","pixel"}, {"computer","silver","lenovo"}, {"phone","gold","iphone"}};
+    char* ruleKey1 = "color";
+    char* ruleValue1 = "silver";
+
+    count = countMatches_1(items1, 3, &itemsColSize, ruleKey1, ruleValue1);
+    printf("count = %d\n", count);
+
+    return 0;
+}
+
+/* Version 2
 Example 1:
 Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
 Output: 1
@@ -65,19 +101,36 @@ Explanation: There are only two items matching the given rule, which are ["phone
 */
 int main() {
     int count = 0;
-    int itemsColSize = 3;
-    //char*** items1 = {{"phone","blue","pixel"}, {"computer","silver","lenovo"}, {"phone","gold","iphone"}};
-    char items1[][3][10] = {{"phone","blue","pixel"}, {"computer","silver","lenovo"}, {"phone","gold","iphone"}};
+    int d1 = 3, d2 = 3, d3 = 10;
+    char static_items1[][3][10] = {{"phone","blue","pixel"}, {"computer","silver","lenovo"}, {"phone","gold","iphone"}};
+    char static_items2[][3][10] = {{"phone","blue","pixel"}, {"computer","silver","phone"}, {"phone","gold","iphone"}};
     char* ruleKey1 = "color";
+    char* ruleKey2 = "type";
     char* ruleValue1 = "silver";
+    char* ruleValue2 = "phone";
 
-    count = countMatches((char ***)items1, 3, &itemsColSize, ruleKey1, ruleValue1);
-    printf("count = %d\n", count);
+    char*** items = (char ***)malloc(d1 * sizeof(char **));
+    for (int i = 0; i < d1; i++){
+        items[i] = (char **)malloc(d2 * sizeof(char *));
+        for (int j = 0; j < d2; j++)
+            items[i][j] = static_items1[i][j];
+    }
+
+    count = countMatches((char ***)items, d1, &d2, ruleKey1, ruleValue1);
+    printf("count1 = %d\n", count);
+
+    for (int i = 0; i < d1; i++){
+        for (int j = 0; j < d2; j++)
+            items[i][j] = static_items2[i][j];
+    }
+
+    count = countMatches((char ***)items, d1, &d2, ruleKey2, ruleValue2);
+    printf("count2 = %d\n", count);
+
+    for (int i = 0; i < d1; i++)
+        free(items[i]);
+    free(items);
 
     return 0;
 }
-
-
-
-
 
