2213a41885f87aa5df75d15eafb3dd251d48b617
Weilin Luo <weilin.william.luo@gmail.com>
2021-12-04 10:57:31 -0800

    0 string lib in progress

---
+++ strings/0_string_lib_funcs.c
@@ -0,0 +1,42 @@
+/*
+My implementation of standard C string library functions.
+0_string_lib_funcs.c
+*/
+#include <stdio.h>
+#include <string.h>
+
+
+/*
+NAME
+       strchr — string scanning operation
+SYNOPSIS
+       #include <string.h>
+       char *strchr(const char *s, int c);
+DESCRIPTION
+       The strchr() function shall locate the first occurrence of c
+       (converted to a char) in the string pointed to by s.  The
+       terminating NUL character is considered to be part of the string.
+RETURN VALUE
+       Upon completion, strchr() shall return a pointer to the byte, or
+       a null pointer if the byte was not found.
+*/
+char *my_strchr(const char *s, int c) {
+
+
+
+
+
+
+}
+
+
+int main() {
+
+
+
+
+
+}
+
+
+
